7fc632669450bc14db9455508b4ad173e9fecc4e
gorzbi <62179493+gorzbi@users.noreply.github.com>
2024-09-11 19:38:59 +0200

    Create Konfiguration.java

---
+++ Szablony/CleanPoM/settings/Konfiguration.java
@@ -0,0 +1,27 @@
+package settings;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+
+public class Konfiguration {
+
+    protected static WebDriver driver;
+
+    @BeforeMethod
+    public void openBrowser() throws InterruptedException {
+
+        driver = DriverFactory.getDriver(DriverType.FIREFOX);
+        driver.manage().window().maximize();
+
+        driver.get("https://www.saucedemo.com/");
+
+    }
+
+    @AfterMethod
+    public void quitBrowser() {
+        driver.quit();
+    }
+
+}
