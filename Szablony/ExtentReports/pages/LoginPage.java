418f86ddf10631aa55454c8fe13f2d0b499c384d
gorzbi <62179493+gorzbi@users.noreply.github.com>
2024-09-16 13:50:42 +0200

    Add files via upload

---
+++ Szablony/ExtentReports/pages/LoginPage.java
@@ -0,0 +1,46 @@
+package pages;
+
+import dane.DaneLogin;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+
+    @FindBy(id = "user-name")
+    private WebElement username;
+
+    @FindBy(id = "password")
+    private WebElement password;
+
+    @FindBy(id = "login-button")
+    private WebElement loginButton;
+
+    @FindBy(id = "error")
+    private WebElement error;
+
+
+    private WebDriver driver;
+    private DaneLogin daneLogin;
+
+    public LoginPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+        this.driver = driver;
+        this.daneLogin = new DaneLogin();
+    }
+
+    public void login(DaneLogin dane){
+        username.clear();
+        username.sendKeys(dane.getUsername());
+        password.clear();
+        password.sendKeys(dane.getPassword());
+        loginButton.click();
+    }
+
+    public void error() {
+        error.click();
+    }
+
+
+}
