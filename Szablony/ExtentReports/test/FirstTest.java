570009998a7adf7ae3622d07d35e916aac9cadf8
gorzbi <62179493+gorzbi@users.noreply.github.com>
2024-09-22 12:40:09 +0200

    Update FirstTest.java

--- Szablony/ExtentReports/test/FirstTest.java
+++ Szablony/ExtentReports/test/FirstTest.java
@@ -3,25 +3,27 @@ package test;
 import dane.DaneLogin;
 import org.testng.annotations.Test;
 import pages.LoginPage;
+import pages.MainPage;
 import settings.Konfiguration;
 
 public class FirstTest extends Konfiguration {
 
-    @Test (priority = 1, testName = "firstTestA", groups= {"kat1","kat2"}, description = "opis1A")
+    @Test (priority = 1, testName = "firstTestA", groups= {"kat1","kat2"}, description = "Test - Correct login")
     public void firstTestA() {
 
         DaneLogin dane = new DaneLogin();
+        MainPage mp = new MainPage(driver);
 
         LoginPage lp = new LoginPage(driver);
-        lp.login(dane);
+        lp.loginAuto(dane);
+        mp.checkAfterCorrectLogin();
     }
 
-    @Test (priority = 1, testName = "firstTestB", groups= {"kat3"}, description = "opis1B")
+    @Test (priority = 1, testName = "firstTestB", groups= {"kat3"}, description = "Test - Validation for wrong user and password")
     public void firstTestB() {
 
-        DaneLogin dane = new DaneLogin();
-
         LoginPage lp = new LoginPage(driver);
-        lp.login(dane);
+        lp.loginManual("wrong", "wrong");
+        lp.checkWrongPasswordErrorMessage();
     }
 }
