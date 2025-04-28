8e6937466a8093ab15a6d82af391f821b0229540
gorzbi <62179493+gorzbi@users.noreply.github.com>
2025-04-28 14:32:01 +0200

    Update DriverFactory.java

--- Szablony/Extent_Reports/src/main/java/settings/DriverFactory.java
+++ Szablony/Extent_Reports/src/main/java/settings/DriverFactory.java
@@ -21,4 +21,23 @@ public class DriverFactory {
             return new EdgeDriver();
         }
     }
+
+        // alternatywne rozwiązanie
+    public static WebDriver getDriver(DriverType type) {
+
+        String error = "Browser does not exist";
+        
+        switch (type) {
+            case DriverType.FIREFOX:
+                WebDriverManager.firefoxdriver().setup();
+                return new FirefoxDriver();
+            case DriverType.CHROME:
+                 WebDriverManager.chromedriver().setup();
+                 return new ChromeDriver();
+            default:
+                System.out.println("Browser does not exist");
+        }
+        // co tu dokładnie zwrócić jeszcze nie wiem
+        return new EdgeDriver();
+    }
 }
